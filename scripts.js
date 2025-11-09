0db76002632a3abcfcf5d2cb732b288e738f945f
Delmika Johnson <delmikajohnson@Delmikas-MacBook-Air.local>
2025-11-09 15:34:58 -0500

    Complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', () => {
+const form = document.querySelector('.newsletter form');
+const emailInput = document.querySelector('#email');
+const messageDiv = document.querySelector('.message');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+    const email = emailInput.value.trim(); 
+
+    if (email) {
+        messageDiv.textContent = `Thank you! Your email address "${email}" has been added to our mailing list!`;
+        messageDiv.style.color = 'white'; 
+        emailInput.value = ''; 
+    } else {
+        
+        messageDiv.textContent = 'Please enter a valid email address.';
+        messageDiv.style.color = '#5A67D8'; 
+    }
+});
+});
